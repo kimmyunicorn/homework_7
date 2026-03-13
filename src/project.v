5ffd573592a2dab1a212b9e763caac23665f4301
Kimberly Camanero <166683958+kimmyunicorn@users.noreply.github.com>
2026-03-12 22:30:14 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,17 +6,17 @@
 `default_nettype none
 
 module full_add (
-    input logic [3:0] a_i,
-    input logic [3:0] b_i,
-    input logic carry_i,
-    output logic carry_o,
-    output logic [3:0] sum_o
+    input wire [3:0] a_i,
+    input wire [3:0] b_i,
+    input wire carry_i,
+    output wire carry_o,
+    output wire [3:0] sum_o
 );
-    logic [4:0] result;
-    always_comb begin
-        result = {1'b0, a_i} + {1'b0, b_i} + {4'b0, carry_i};
-        sum_o = result[3:0];
-        carry_o = result[4];
+    wire [4:0] result;
+    assign result = {1'b0, a_i} + {1'b0, b_i} + {4'b0, carry_i};
+    assign sum_o = result[3:0];
+    assign carry_o = result[4];
+
     end
 endmodule
 
