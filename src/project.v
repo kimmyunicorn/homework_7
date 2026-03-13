07f9c5ed0e3536b0357fbfeb97f80bc2f8262613
Kimberly Camanero <166683958+kimmyunicorn@users.noreply.github.com>
2026-03-12 21:42:23 -0700

    Update project.v
    made this on another ide but did not want to push so I am editing directly on github.

--- src/project.v
+++ src/project.v
@@ -1,27 +1,49 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Kimberly Camanero
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module full_add (
+    input logic [3:0] a_i,
+    input logic [3:0] b_i,
+    input logic carry_i,
+    output logic carry_o,
+    output logic [3:0] sum_o
 );
+    logic [4:0] result;
+    always_comb begin
+        result = {1'b0, a_i} + {1'b0, b_i} + {4'b0, carry_i};
+        sum_o = result[3:0];
+        carry_o = result[4];
+    end
+endmodule
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+module tt_um_example (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
+);
+    wire [3:0] sum;
+    wire carry_out;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    full_add kimmys_adder(
+        .a_i(ui_in[3:0]),
+        .b_i(uio_in[3:0]),
+        .carry_i(ui_in[4]),
+        .sum_o(sum),
+        .carry_o(carry_out)
+    );
 
+    assign uo_out = {3'b0, carry_out, sum};
+    assign uio_out = 0;
+    assign uio_oe = 0;
+
+    wire _unused = &{ena, clk, rst_n, ui_in[7:5], uio_in[7:4], 1'b0};
 endmodule
